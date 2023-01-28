b73fb80c120d6853d96b8c1ed7a73a70013f711a
aldiresaldim <aldi.825200008@stu.untar.ac.id>
2023-01-28 07:40:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Function for show slide  */
+
+// we have to pass n as an parameter in function
+// we'll get the value of n by calling function and passing slideIndex as an argument
+
+  // let's write condition to loop a slideshow
+  // when n is greater then slides.length then value of slideIndex should be 1
+
+  // when n is less then 1 then value of slideIndex should be equal to slides.length
+
+  /* hiding all images by default using display none  */
